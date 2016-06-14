bee03b3a4bf88ffa1e71d731e076bac9259298a1
Dustin Milam <darklordsushi@yahoo.com>
2016-06-14 18:28:04 -0400

    added api keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,8 @@
 ShopifyApp.configure do |config|
-  config.api_key = ""
-  config.secret = ""
-  config.redirect_uri = "<redirect_uri>"
-  config.scope = "read_orders, read_products"
+  config.api_key = "e923492233842ad33bb2d54987e3a3fe"
+  config.secret = "647cd43249ebecfcf2b764a1386f0635"
+  config.redirect_uri = "https://cart-timer.herokuapp.com/auth/shopify/callback/"
+  # config.callback_url = "https://shopifytestasp.herokuapp.com/auth/shopify/callback/"
+  config.scope = "read_orders, read_products, read_content"
   config.embedded_app = true
 end
