5067fe63e5d3b0fcc73c8567c331f78638c8b882
Dustin Milam <darklordsushi@yahoo.com>
2016-06-15 16:38:48 -0400

    shop meathod to create an option instance as it creates a shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,13 @@ class Shop < ActiveRecord::Base
   include ShopifyApp::Shop
 
   has_one :option
+  before_create :build_options_set
+
+  private
+
+  	def build_options_set
+  		build_option
+  		true
+  	end
+
 end
