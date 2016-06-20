51da8b7137b5aed4477c0e1e5b3fc864fda5f55a
Dustin Milam <darklordsushi@yahoo.com>
2016-06-20 17:04:52 -0400

    Added message option fields and whitelisted them

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,7 +30,8 @@ class HomeController < AuthenticatedController
 		def option_params
 			params.require(:option).permit(:enable, :custom_css, :timer_label_text, :timer_warning_message, :timer_expired_message, :description,
                                      :header_color, :header_font_size, :header_border_color, :header_background_color, :header_border_radius, :header_padding, :header_margin,
-                                     :quick_view_color, :quick_view_font_size, :quick_view_border_color, :quick_view_background_color, :quick_view_border_radius, :quick_view_padding, :quick_view_margin)
+                                     :quick_view_color, :quick_view_font_size, :quick_view_border_color, :quick_view_background_color, :quick_view_border_radius, :quick_view_padding, :quick_view_margin,
+                                     :message_color, :message_font_size, :message_border_color, :message_background_color, :message_border_radius, :message_padding, :message_margin)
 		end
 
 end
