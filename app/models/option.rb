6f8b2d5123f0d06111c9f4e1383842cb797f0fab
Dustin Milam <darklordsushi@yahoo.com>
2016-10-17 13:41:20 -0400

    Added validations for button text and json method for Option

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,10 @@
 class Option < ActiveRecord::Base
 	belongs_to :shop
+
+	validates :button_1_text, presence: true
+	validates :button_2_text, presence: true
+
+	def json
+		self.attributes.except("shop_id", "created_at", "updated_at").to_json
+	end
 end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -11,8 +11,14 @@ describe Option do
 		FactoryGirl.create(:option).should be_valid
   end
   it "has a valid button text" do
-  	FactoryGirl.build(:contact, button_1_text: nil).should_not be_valid
-  	FactoryGirl.build(:contact, button_2_text: nil).should_not be_valid
+  	FactoryGirl.build(:option, button_1_text: nil).should_not be_valid
+  	FactoryGirl.build(:option, button_2_text: nil).should_not be_valid
   end
-  it "returns all user editable data as JSON"
+  it "returns only user editable data as JSON" do
+  	shop = FactoryGirl.create(:shop)
+  	option_json = FactoryGirl.create(:option).json
+  	option_json.include?("shop_id").should be false 
+  	option_json.include?("created_at").should be false
+  	option_json.include?("updated_at").should be false
+  end
 end
