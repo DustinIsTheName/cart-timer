07377a047556bc84e6ff00733f76ef4970588ba2
Dustin Milam <darklordsushi@yahoo.com>
2016-10-20 09:40:13 -0400

    Fixed options model, added feature tests and removed controller tests

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,11 +1,6 @@
 class Option < ActiveRecord::Base
 	belongs_to :shop
 
-	before_save :default_values
-
-	# validates :button_1_text, presence: true
-	# validates :button_2_text, presence: true
-
 	def json
 		self.attributes.except("shop_id", "created_at", "updated_at").to_json
 	end
--- spec/controllers/home_spec.rb
+++ spec/controllers/home_spec.rb
@@ -1,35 +1,42 @@
-require 'spec_helper'
-require 'shopify_app'
+# require 'spec_helper'
+# require 'shopify_app'
 
-describe HomeController do
+# describe HomeController do
 
-	describe "GET index" do
-		# it "finds a shop" do
-		# 	request.env['HTTPS'] = 'on'
-		# 	shop = FactoryGirl.create(:shop)
-		# 	get :index
-		# 	assigns(:shop).should eq(shop)
-		# end
+# 	describe "GET index" do
+# 		# it "finds a shop" do
+# 		# 	request.env['HTTPS'] = 'on'
+# 		# 	shop = FactoryGirl.create(:shop)
+# 		# 	get :index
+# 		# 	assigns(:shop).should eq(shop)
+# 		# end
 
-		# it "finds a shop's options" do
-		# 	shop = FactoryGirl.create(:shop)
-		# end
+# 		# it "finds a shop's options" do
+# 		# 	shop = FactoryGirl.create(:shop)
+# 		# end
 
-	end
+# 	end
 
-	describe "PATCH update" do
+# 	describe "PATCH update" do
 
-	end
+# 	end
 
-	describe "GET install_instructions" do
-		it 'renders install_instructions.html.erb' do
-			get :install_instructions
-			response.should render_template("install_instructions")
-		end
-	end
+# 	describe "GET install_instructions" do
+# 		it 'renders install_instructions.html.erb' do
+# 			user_installs_app 'cart-timer.myshopify.com'
+# 			# get :index
+# 			response.should render_template("index")
+# 		end
+# 	end
 
-	describe "GET timer-js" do
+# 	describe "GET timer-js" do
 
-	end
+# 	end
 
-end
+#   def user_installs_app(shopify_url)
+#     visit login_path
+#     fill_in 'shop', with: shopify_url
+#     click_button 'Install'
+#   end
+
+# end
